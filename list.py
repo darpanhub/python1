1e9c169960758a656eaad541fe6fb86c129db89f
darpanhub <62980556+darpanhub@users.noreply.github.com>
2020-04-18 07:14:37 +0530

    list

--- list.py
+++ list.py
@@ -1,2 +1,42 @@
-student_list=[1,45,6,6,7]
-print(student_list)
+# student_list=[1,45,6,6,7]
+# print(student_list)
+
+
+# print('Welcome User')
+# admin_list = ['manish' , 'darpan' , 'sandeep', 'mohammed']
+# print('hey user, Please enter your name for verification')
+
+# name = input()
+
+# if name in admin_list:
+#     print('hey ' + name + ', happy to see you Welcome back')
+# else:
+#     print('Hey ' + name + ',You are Not a member please register to continue')
+
+# import random
+# print('enter number')
+# num = input()
+
+# print('Your OTP is :')
+# for i in range(6):
+#     otp = random.choice(num)
+#     print(otp, end='')  
+
+# user register
+# user_database = ['darpan','manish','mohammed','sandeep']
+# usernames_list = ['darpan','manish','mohammed','sandeep']
+
+# print('Enter First Name')
+# first_name = input().strip().lower()
+# print('Enter last Name')
+# last_name = input().strip().lower()
+# print('Enter User Name')
+# user_name = input().strip().lower()
+# print('Enter your password')
+# password = input().strip().lower()
+# password2 = input().strip().lower()
+
+# if password == password2:
+#     user_database['usernames_list'].insert(user_name)
+#     user_database[user_name +'_password']= password
+#     print('hey {} Your Account is Created'.format(user_name))
