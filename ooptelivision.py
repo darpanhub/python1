7439ea507e1724d4982eb5fdba27982403b55503
darpanhub <62980556+darpanhub@users.noreply.github.com>
2020-04-23 12:28:39 +0530

    oop television

---
+++ ooptelivision.py
@@ -0,0 +1,13 @@
+class telivision():
+    def __init__(self):
+        print('Welcome to Television showroom')
+
+    def wallhang(self):
+        print('smart wall hanging tv')
+
+    def screen(self):
+        print('screen size is 32 inchs.')
+
+    def resolution(self):
+        print('4k resolution is available ')
+
